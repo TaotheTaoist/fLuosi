5e0b180ea926bf9c0e14a3d455303f201ae7d6ea
TaotheTaoist <daowuchen@hotmail.com>
2024-07-11 01:37:56 -0700

    ?

--- lib/baxi_page.dart
+++ lib/baxi_page.dart
@@ -1,4 +1,3 @@
-import 'package:auto_size_text/auto_size_text.dart';
 import 'package:flutter/material.dart';
 import 'package:learn1/calendar/Lunar.dart' as lunar;
 import 'new_member.dart'; // Import NewMember class
@@ -48,44 +47,151 @@ class _BaziPageState extends State<BaziPage> {
 
     lunar.Lunar lunarDate = lunar.Lunar.fromDate(combinedDateTime);
 
-    // Helper function to build table rows
-    TableRow buildRow(List<Widget> cells) {
-      return TableRow(
-        children: cells.map((cell) {
-          return Padding(
-            padding: const EdgeInsets.all(4.0),
-            child: Center(child: cell),
-          );
-        }).toList(),
-      );
-    }
-
     Widget buildCell(String text) {
-      return AutoSizeText(
-        text,
-        maxLines: 1,
-        minFontSize: 8,
-        maxFontSize: 14,
-        overflow: TextOverflow.ellipsis,
+      return Container(
+        padding: const EdgeInsets.all(8.0),
+        alignment: Alignment.center,
+        child: Text(
+          text,
+          style: const TextStyle(fontSize: 14),
+          overflow: TextOverflow.ellipsis,
+        ),
       );
     }
 
     Widget buildDropdownCell() {
-      return DropdownButton<String>(
-        value: selectedValue,
-        onChanged: (String? newValue) {
-          setState(() {
-            selectedValue = newValue!;
-          });
-        },
-        items: dropDownOptions.map<DropdownMenuItem<String>>((String value) {
-          return DropdownMenuItem<String>(
-            value: value,
-            child: Text(value),
-          );
-        }).toList(),
+      return Container(
+        padding: const EdgeInsets.all(8.0),
+        alignment: Alignment.center,
+        child: Container(
+          width: 40, // Adjust the width as needed
+          child: DropdownButton<String>(
+            value: selectedValue,
+            isExpanded: true, // Ensures the dropdown takes the width of its container
+            onChanged: (String? newValue) {
+              setState(() {
+                selectedValue = newValue!;
+              });
+            },
+            items: dropDownOptions.map<DropdownMenuItem<String>>((String value) {
+              return DropdownMenuItem<String>(
+                value: value,
+                child: Text(
+                  value,
+                  style: const TextStyle(fontSize: 12), // Adjust the font size as needed
+                ),
+              );
+            }).toList(),
+          ),
+        ),
       );
     }
+
+    TableRow buildRow(List<Widget> cells) {
+      return TableRow(
+        children: cells,
+        decoration: const BoxDecoration(
+          border: Border(
+            bottom: BorderSide(color: Colors.black),
+          ),
+        ),
+      );
+    }
+
+    List<TableRow> tableRows = [
+      buildRow([
+        buildCell('日期'),
+        buildCell('時柱'),
+        buildCell('日柱'),
+        buildCell('月柱'),
+        buildCell('年柱'),
+        buildCell('大運'),
+        buildCell('流年'),
+        buildCell('流月'),
+        buildDropdownCell(),
+      ]),
+      buildRow([
+        buildCell('26岁'),
+        buildCell('27岁'),
+        buildCell('7月'),
+        buildCell('3日'),
+        buildCell(''),
+        buildCell(''),
+        buildCell(''),
+        buildCell(''),
+        buildCell(''),
+      ]),
+      buildRow([
+        buildCell('天干'),
+        buildCell(lunarDate.getDayGan()),
+        buildCell(lunarDate.getDayGan()),
+        buildCell(lunarDate.getMonthGan()),
+        buildCell(lunarDate.getYearGan()),
+        buildCell(''),
+        buildCell(''),
+        buildCell(''),
+        buildCell(''),
+      ]),
+      buildRow([
+        buildCell('地支'),
+        buildCell(lunarDate.getDayZhi()),
+        buildCell(lunarDate.getDayZhi()),
+        buildCell(lunarDate.getMonthZhi()),
+        buildCell(lunarDate.getYearZhi()),
+        buildCell(''),
+        buildCell(''),
+        buildCell(''),
+        buildCell(''),
+      ]),
+      buildRow([
+        buildCell('流年'),
+        buildCell('天干'),
+        buildCell('地支'),
+        buildCell(''),
+        buildCell(''),
+        buildCell(''),
+        buildCell(''),
+        buildCell(''),
+        buildCell(''),
+      ]),
+      buildRow([
+        buildCell(''),
+        buildCell(lunarDate.getYearGan()),
+        buildCell(lunarDate.getYearZhi()),
+        buildCell(''),
+        buildCell(''),
+        buildCell(''),
+        buildCell(''),
+        buildCell(''),
+        buildCell(''),
+      ]),
+    ];
+
+    List<Widget> longRow = [
+      buildCell('神煞'),
+      buildCell('k'),
+      buildCell('/'),
+      buildCell('/'),
+      buildCell('/'),
+      buildCell('/'),
+      buildCell('?'),
+      buildCell('?'),
+      buildCell('/'),
+      buildCell('/'),
+      buildCell('?'),
+      buildCell('?'),
+      buildCell('?'),
+      buildCell('/'),
+      buildCell('?'),
+      buildCell('?'),
+      buildCell('?'),
+      buildCell('?'),
+      buildCell('?'),
+      buildCell('/'),
+      buildCell('?'),
+      buildCell('?'),
+      buildCell('?'),
+    ];
 
     return Scaffold(
       appBar: AppBar(
@@ -95,121 +201,16 @@ class _BaziPageState extends State<BaziPage> {
         child: Padding(
           padding: const EdgeInsets.all(16.0),
           child: Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
             children: [
-              const SizedBox(height: 20),
-              Container(
-                width: MediaQuery.of(context).size.width,
-                child: Table(
-                  border: TableBorder.all(color: Colors.black),
-                  children: [
-                    buildRow([
-                      buildCell('日期'),
-                      buildCell('時柱'),
-                      buildCell('日柱'),
-                      buildCell('月柱'),
-                      buildCell('年柱'),
-                      buildCell('大運'),
-                      buildCell('流年'),
-                      buildCell('流月'),
-                      buildDropdownCell(),
-                    ]),
-                    buildRow([
-                      buildCell('26岁'),
-                      buildCell('27岁'),
-                      buildCell('7月'),
-                      buildCell('3日'),
-                      buildCell(''),
-                      buildCell(''),
-                      buildCell(''),
-                      buildCell(''),
-                      buildCell(''),
-                    ]),
-                    buildRow([
-                      buildCell('天干'),
-                      buildCell(lunarDate.getDayGan()),
-                      buildCell(lunarDate.getDayGan()),
-                      buildCell(lunarDate.getMonthGan()),
-                      buildCell(lunarDate.getYearGan()),
-                      buildCell(''),
-                      buildCell(''),
-                      buildCell(''),
-                      buildCell(''),
-                    ]),
-                    buildRow([
-                      buildCell('地支'),
-                      buildCell(lunarDate.getDayZhi()),
-                      buildCell(lunarDate.getDayZhi()),
-                      buildCell(lunarDate.getMonthZhi()),
-                      buildCell(lunarDate.getYearZhi()),
-                      buildCell(''),
-                      buildCell(''),
-                      buildCell(''),
-                      buildCell(''),
-                    ]),
-                  ],
-                ),
+              Table(
+                border: TableBorder.all(color: Colors.black),
+                children: tableRows,
               ),
               const SizedBox(height: 20),
-              Container(
-                width: MediaQuery.of(context).size.width,
-                child: Table(
-                  border: TableBorder.all(color: Colors.black),
-                  children: [
-                    buildRow([
-                      buildCell('流年'),
-                      buildCell('天干'),
-                      buildCell('地支'),
-                      buildCell(''),
-                      buildCell(''),
-                      buildCell(''),
-                      buildCell(''),
-                      buildCell(''),
-                      buildCell(''),
-                    ]),
-                    buildRow([
-                      buildCell(''),
-                      buildCell(lunarDate.getYearGan()),
-                      buildCell(lunarDate.getYearZhi()),
-                      buildCell(''),
-                      buildCell(''),
-                      buildCell(''),
-                      buildCell(''),
-                      buildCell(''),
-                      buildCell(''),
-                    ]),
-                  ],
-                ),
-              ),
-              const SizedBox(height: 20),
-              Container(
-                width: MediaQuery.of(context).size.width,
-                child: Table(
-                  border: TableBorder.all(color: Colors.black),
-                  children: [
-                    buildRow([
-                      buildCell('神煞'),
-                      buildCell(''),
-                      buildCell(''),
-                      buildCell(''),
-                      buildCell(''),
-                      buildCell(''),
-                      buildCell(''),
-                      buildCell(''),
-                      buildCell(''),
-                    ]),
-                    buildRow([
-                      buildCell(''),
-                      buildCell(''),
-                      buildCell(''),
-                      buildCell(''),
-                      buildCell(''),
-                      buildCell(''),
-                      buildCell(''),
-                      buildCell(''),
-                      buildCell(''),
-                    ]),
-                  ],
+              SingleChildScrollView(
+                scrollDirection: Axis.horizontal,
+                child: Row(
+                  children: longRow,
                 ),
               ),
             ],
